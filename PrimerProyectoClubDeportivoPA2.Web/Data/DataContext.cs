9d6c2b88aa0d88e97e1833b323b995b513e84338
manuel23arredondo <arredondo23manuel@gmail.com>
2021-09-02 20:47:55 -0500

    Creación del proyecto de primer parcial, con creación de la clase DataContext, CUser, junto con la instalación de paquetes.

---
+++ PrimerProyectoClubDeportivoPA2.Web/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿
+namespace PrimerProyectoClubDeportivoPA2.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using PrimerProyectoClubDeportivoPA2.Web.Data.Entities;
+
+    public class DataContext : IdentityDbContext<User>
+
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+
+        {
+
+        }
+
+    }
+
+
+}
