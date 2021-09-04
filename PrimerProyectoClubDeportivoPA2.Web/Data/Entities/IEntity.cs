b4501f501b95eaf093eb95ef7ad85819b0452e0a
Samuel Díaz <samuelsd8@gmail.com>
2021-09-04 16:06:35 -0500

    Se agregó la interfaz IEntity

---
+++ PrimerProyectoClubDeportivoPA2.Web/Data/Entities/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerProyectoClubDeportivoPA2.Web.Data.Entities
+{
+    interface IEntity
+    {
+        public interface IEntity
+        {
+            int Id { get; set; }
+        }
+    }
+}
