6db6e30be4cecaf848f9aa49a9ec7b1d31b7e829
manuel23arredondo <arredondo23manuel@gmail.com>
2021-09-14 16:58:57 -0500

    Creacion clase PermissionAccess. Junto con relación a Sport.

---
+++ PrimerProyectoClubDeportivoPA2.Web/Data/Entities/PermissionAccess.cs
@@ -0,0 +1,16 @@
+﻿namespace PrimerProyectoClubDeportivoPA2.Web.Data.Entities
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    public class PermissionAccess : IEntity
+    {
+        [Display(Name = "Clave")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        [MaxLength(20, ErrorMessage = "Debe introducir máximo {1} caracteres")]
+        [Display(Name = "Permiso para acceder")]
+        public string Name { get; set; }
+
+        public Sport Sport { get; set; }
+    }
+}
