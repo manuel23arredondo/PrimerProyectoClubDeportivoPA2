ed4c059c82c2390af35174b59597ee60da000367
manuel23arredondo <arredondo23manuel@gmail.com>
2021-09-14 17:17:28 -0500

    Creación de TrainingSession. Relación de TrainingSession con Coach

---
+++ PrimerProyectoClubDeportivoPA2.Web/Data/Entities/TrainingSession.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimerProyectoClubDeportivoPA2.Web.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    public class TrainingSession
+    {
+        [Display(Name = "Clave")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        [MaxLength(20, ErrorMessage = "Debe introducir máximo {1} caracteres")]
+        [Display(Name = "Sesión de entrenamiento")]
+        public string Name { get; set; }
+
+        public Coach Coach { get; set; }
+    }
+}
