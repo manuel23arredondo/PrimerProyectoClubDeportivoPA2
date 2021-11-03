cf6f5dc7ad11b65839ba681e33b5757b1695c090
Samuel Díaz <samuelsd8@gmail.com>
2021-11-02 21:07:14 -0600

    Se modificó el método Edit en FacilitiesController y se modificó la vista Edit de Facilities

--- PrimerProyectoClubDeportivoPA2.Web/Controllers/FacilitiesController.cs
+++ PrimerProyectoClubDeportivoPA2.Web/Controllers/FacilitiesController.cs
@@ -1,5 +1,6 @@
 ﻿namespace PrimerProyectoClubDeportivoPA2.Web.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using PrimerProyectoClubDeportivoPA2.Web.Data;
@@ -8,6 +9,7 @@
     using PrimerProyectoClubDeportivoPA2.Web.Models;
     using System.Linq;
     using System.Threading.Tasks;
+    [Authorize(Roles = "Admin")]
     public class FacilitiesController : Controller
     {
         private readonly DataContext dataContext;
@@ -71,6 +73,7 @@
             return View(model);
         }
 
+        [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -78,50 +81,44 @@
                 return NotFound();
             }
 
-            var facility = await dataContext.Facilities.FindAsync(id);
+            var facility = await this.dataContext.Facilities.FirstOrDefaultAsync(p => p.Id == id);
             if (facility == null)
             {
                 return NotFound();
             }
-            return View(facility);
+            var model = new FacilityViewModel
+            {
+                Id = facility.Id,
+                Code = facility.Code,
+                Name = facility.Name,
+                Description = facility.Description,
+                ImageUrl = facility.ImageUrl
+            };
+            return View(model);
         }
 
-        // POST: Facilities/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Code,Name,Description")] Facility facility)
+        public async Task<IActionResult> Edit(FacilityViewModel model)
         {
-            if (id != facility.Id)
-            {
-                return NotFound();
-            }
-
             if (ModelState.IsValid)
             {
-                try
+                var facility = new Facility
                 {
-                    dataContext.Update(facility);
-                    await dataContext.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!FacilityExists(facility.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                    Id = model.Id,
+                    Code = model.Code,
+                    Name = model.Name,
+                    Description = model.Description,
+                    ImageUrl = (model.ImageFile != null ? await imageHelper.UploadImageAsync(model.ImageFile, model.Name, "facilities") : model.ImageUrl)
+                };
+                
+                this.dataContext.Update(facility);
+                await this.dataContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(facility);
+            return View(model);
         }
 
-        // GET: Facilities/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
